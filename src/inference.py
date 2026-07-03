28719597cdf3e8fc1ad0816a6b7114859ccffded
meatyaw <richardvalve2@gmail.com>
2026-07-03 12:51:40 +0700

    inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,48 +1,39 @@
 import os
-import boto3
 import joblib
-
+import boto3
+import pandas as pd
 import streamlit as st
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
-    region_name=st.secrets["AWS_DEFAULT_REGION"]
-)
+
 class CreditScorePredictor:
 
     def __init__(self):
 
         self.bucket = "credit-score-md"
-
         self.key = "best_model.pkl"
-
         self.local_model = "best_model.pkl"
 
-        if not os.path.exists(self.local_model):
+        self.s3 = boto3.client(
+            "s3",
+            aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
+            region_name=st.secrets["AWS_DEFAULT_REGION"]
+        )
 
+        if not os.path.exists(self.local_model):
             self.download_model()
 
         self.model = joblib.load(self.local_model)
 
     def download_model(self):
 
-        s3 = boto3.client("s3")
-
-        s3.download_file(
-
+        self.s3.download_file(
             self.bucket,
-
             self.key,
-
             self.local_model
-
         )
 
     def predict(self, data):
-
-        import pandas as pd
 
         df = pd.DataFrame([data])
 
