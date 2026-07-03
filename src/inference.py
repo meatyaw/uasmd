3506db14eb4621e5d1bcce2e95dc09e88e336801
meatyaw <richardvalve2@gmail.com>
2026-07-03 12:43:31 +0700

    inference.py

--- src/inference.py
+++ src/inference.py
@@ -2,7 +2,14 @@ import os
 import boto3
 import joblib
 
+import streamlit as st
 
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
+    region_name=st.secrets["AWS_DEFAULT_REGION"]
+)
 class CreditScorePredictor:
 
     def __init__(self):
